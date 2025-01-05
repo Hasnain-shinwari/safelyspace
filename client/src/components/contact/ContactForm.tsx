205710c43ae0d5e9d1e3e7fc0c32f6a03509dec0
Muhammad Abdullah <mabdullah.ui2@gmail.com>
2025-01-05 16:05:10 +0500

    Finalised the entire Contact Form

--- client/src/components/contact/ContactForm.tsx
+++ client/src/components/contact/ContactForm.tsx
@@ -1,35 +1,54 @@
-import React from 'react'
+import React from "react";
 
 const ContactForm = () => {
-  return (
-    <div>
-      <form action="" className='flex mb-6'>
-        <div className="flex flex-col mr-16">
-          <label htmlFor="FullName">Full Name</label>
-          <input type="text" id='FullName' placeholder='Muhammad Ali' className='py-2 pr-64 pl-2 border border-gray-500 bg-transparent' />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="Email">Email</label>
-          <input type="text" id='Email' placeholder='name@example.com' className='py-2 pr-64 pl-2 border border-gray-500 bg-transparent'/>
-        </div>
-      </form>
-      <form action="" className='flex'>
-      <div className="flex flex-col mr-16">
-          <label htmlFor="Dropdown">What are you interested in?</label>
-          <select name="cars" id="Dropdown">
-            <option value="volvo">Volvo</option>
-            <option value="saab">Saab</option>
-            <option value="mercedes">Mercedes</option>
-            <option value="audi">Audi</option>
-          </select>
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="Email">Email</label>
-          <input type="text" id='Email' placeholder='name@example.com' className='py-2 pr-64 pl-2 border border-gray-500 bg-transparent'/>
-        </div>
-      </form>
-    </div>
-  )
-}
+	return (
+		<div>
+			<form action="" className="flex flex-col mb-6">
+				<div className="flex justify-between mb-6">
+					<div className="flex flex-col">
+						<label htmlFor="FullName">Full Name</label>
+						<input
+							type="text"
+							id="FullName"
+							placeholder="Dr. Muhammad Nawaz"
+							className="py-2 pr-44 pl-2 border border-gray-500 bg-transparent mb-6"
+						/>
+						<label htmlFor="Dropdown">What are you interested in?</label>
+						<select name="Interested" id="Dropdown" className="py-2 pr-44 pl-2 border border-gray-500 bg-transparent">
+							<option value="performance">Related to Performance</option>
+							<option value="detection">ML isn't detecting properly.</option>
+							<option value="attachment">I can't attach the files.</option>
+						</select>
+					</div>
+					<div className="flex flex-col">
+						<label htmlFor="Email">Email</label>
+						<input
+							type="text"
+							id="Email"
+							placeholder="name@example.com"
+							className="py-2 pr-44 pl-2 border border-gray-500 bg-transparent mb-6"
+						/>
+						<label htmlFor="issues">Issue you are facing</label>
+						<select name="Interested" id="Dropdown" className="py-2 pr-44 pl-2 border border-gray-500 bg-transparent">
+							<option value="performance">Related to Performance</option>
+							<option value="detection">ML isn't detecting properly.</option>
+							<option value="attachment">I can't attach the files.</option>
+						</select>
+					</div>
+				</div>
+				<div className="">
+					<form action="" className="flex flex-col">
+						<label htmlFor="message">Message</label>
+						<input
+							type="text"
+							placeholder="Type your message here"
+							className="pl-2 pt-2 pb-44 pr-72 border border-gray-500 bg-transparent"
+						/>
+					</form>
+				</div>
+			</form>
+		</div>
+	);
+};
 
-export default ContactForm
+export default ContactForm;
